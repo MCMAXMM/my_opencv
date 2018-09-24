b3582294719a5282b8bef56e4dc503428211df5f
MCMAXMM <32216793+MCMAXMM@users.noreply.github.com>
2018-09-24 15:25:31 +0800

    Update open_cv_hello_world.py

--- open_cv_hello_world.py
+++ open_cv_hello_world.py
@@ -28,3 +28,51 @@ get_video()读取video
 src=cv.imread(r"D:\pythonrun\2018613pytorch\faces\252418361_440b75751b.jpg")
 src=cv.cvtColor(src,cv.COLOR_RGB2GRAY)#改成灰度图像
 cv.imwrite("./hello.jpg",src)
+
+#改变色彩空间
+# gray=cv.cvtColor(src,cv.COLOR_RGB2GRAY)
+# cv.imshow("gray",gray)
+# print(gray.shape)
+# hsv=cv.cvtColor(src,cv.COLOR_RGB2HSV)
+# print(hsv.shape)
+# cv.imshow("hsv",hsv)#h:0-180,s:0-255,v:0-255
+# yuv=cv.cvtColor(src,cv.COLOR_RGB2YUV)
+# cv.imshow("yuv",yuv)
+# print(yuv.shape)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
+
+#图像色彩的捕捉，也可以用于video
+# hsv=cv.cvtColor(src,cv.COLOR_RGB2HSV)
+# lower_hsv=np.array([37,43,46])#捕捉图像中的绿色
+# upper_hsv=np.array([77,255,255])
+# mask=cv.inRange(hsv,lowerb=lower_hsv,upperb=upper_hsv)
+# #使用inRange捕捉绿色，并将图像转换成二值图像
+# cv.imshow("hsv",mask)
+# cv.imshow("rgb",src)
+# c=cv.waitKey(0)
+# cv.destroyAllWindows()
+
+#图像的分离与合并
+#r,g,b=cv.split(src)
+# cv.imshow("r",r)
+# cv.imshow("g",g)
+# cv.imshow("b",b)
+# cv.imshow("merge",cv.merge([r,b,g]))
+# cv.waitKey(0)
+# cv.destroyAllWindows()
+
+#图像相关的运算(算数运算和几何运算)
+# cv.subtract()
+# cv.add()
+# cv.multiply()
+# cv.divide()
+#各个通道的均值和方差
+mean=cv.mean(src)#返回三个通道的均值
+mean,std=cv.meanStdDev(src)#返回两个array分别是三个通道的均值和方差
+#逻辑运算
+cv.bitwise_not()
+cv.bitwise_and()
+cv.bitwise_or()
+cv.bitwise_xor()#亦或
+
